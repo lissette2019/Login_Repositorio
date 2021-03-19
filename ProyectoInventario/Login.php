b944f1138b0eb653f83828816ce178536bd9ee6f
Claudia Leiva <50220959+lissette2019@users.noreply.github.com>
2021-03-18 23:58:13 -0600

    Add files via upload
    En esta rama realizare las modificaciones correspondientes del login

--- ProyectoInventario/Login.php
+++ ProyectoInventario/Login.php
@@ -3,19 +3,35 @@
     <html>
         <head>
             <meta charset="utf-8">
-            <title>Login</title>
-            <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-            <link rel="stylesheet" href="assets/css/style.css">
+                <!-- Latest compiled and minified CSS -->
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+
+            <!-- Optional theme -->
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap-theme.min.css" integrity="sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ" crossorigin="anonymous">
+
+            <!-- Latest compiled and minified JavaScript -->
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
+            <link rel="stylesheet" type="text/css" href="assets/css/style.css">       
         </head>
         <body>
+            <div class="container" id='login'>
+                <div class="panel panel-primary">
+                    <div class="panel-heading text-center">
+                    <h1>Login</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="panel-body">
+                <h4>Usuario</h4>
+                <input class="form-control" name="email" type="text" placeholder="coloca tu correo"><br>
+                <h4>contraseña</h4>
+                <input class="form-control" name="password" type="password" placeholder="ingresa tu contraseña"><br>
+            </div>
+            <div class="panel-footer">
+                <button class="btn-info">Ingresar</button>
+                <button class="btn-info">Registrate</a></button>
+            </div>
 
-            <h1>Login</h1>
-            <span>or <a href="Registro.php">Registrate</a></span>
 
-            <form action="Login.php" method="POST">
-            <input name="email" type="text" placeholder="coloca tu correo">
-            <input name="password" type="password" placeholder="ingresa tu contraseña">
-            <input type="submit" value="Submit">
-            </form>
         </body>
     </html>
