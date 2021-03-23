5ff53c635bd06fe949589936864694d1c82db0d4
claudia.2015lissette@gmail.com <50220959+lissette2019@users.noreply.github.com>
2021-03-22 23:04:53 -0600

    creación de carpeta imágenes

--- ProyectoInventario/Login.php
+++ ProyectoInventario/Login.php
@@ -1,97 +1,88 @@
 <?php
-            //inicializar sesion
+        //inicializar sesion
         session_start();
 
         if (isset($_SESSION['user_id'])) {
-            header('Location: /php-Login');
+            header('Location: /php-login');
         }
         require 'BaseDatos.php';
 
+        //verificar los campos con la condicion
         if (!empty($_POST['email']) && !empty($_POST['password'])) {
-            $records = $conn->prepare('SELECT id, email, password FROM Usuario WHERE email = :email');
+            $records = $conn->prepare('SELECT id, email, password FROM usuarios WHERE email = :email');
             $records->bindParam(':email', $_POST['email']);
             $records->execute();
             $results = $records->fetch(PDO::FETCH_ASSOC);
 
             $message = '';
 
+                //validar el usuario y si no se encuentra vacio
             if (count($results) > 0 && password_verify($_POST['password'], $results['password'])) {
             $_SESSION['user_id'] = $results['id'];
-            header("Location: /php-Login");
+            header("Location: /php-login");
             } else {
             $message = 'Lo siento, esas credenciales no coinciden';
             }
         }
 
 ?>
-
 <!DOCTYPE html>
-
     <html>
         <head>
             <meta charset="utf-8">
-                <!-- Latest compiled and minified CSS -->
-            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
-
-            <!-- Optional theme -->
-            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap-theme.min.css" integrity="sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ" crossorigin="anonymous">
-
-            <!-- Latest compiled and minified JavaScript -->
-            <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
+               
             <link rel="stylesheet" type="text/css" href="assets/css/style.css">       
         </head>
         <body>
-        <body>
+        <?php if(!empty($message)): ?>
+            <p> <?= $message ?></p>
+            <?php endif; ?>
+                <header id="main-header">
+                
+                <a id="logo-header" href="/assets/Imagenes/LOGO INVENTARIO.png">
+                    <span class="site-name">HOSPITAL MILITAR CENTRAL</span><br>
+                    <span class="site-desc">Diseño de Inventario</span>
+                </a> <!-- / #logo-header -->
 
-<header id="main-header">
+                <nav>
+                    <ul>
+                        <li><a href="index.php">Inicio</a></li>
+                        <li><a href="Registro.php">Registro</a></li>
+                        <li><a href="CerrarSesion.php">Salir</a></li>
+                    </ul>
+                </nav><!-- / nav -->
 
-<a id="logo-header" href="/assets/Imagenes/LOGO INVENTARIO.png">
-    <span class="site-name">HOSPITAL MILITAR CENTRAL</span><br>
-    <span class="site-desc">Diseño de Inventario</span>
-</a> <!-- / #logo-header -->
+            </header><!-- / #main-header -->
 
-<nav>
-    <ul>
-        <li><a href="index.php">Inicio</a></li>
-        <li><a href="Registro.php">Registro</a></li>
-        <li><a href="CerrarSesion.php">Salir</a></li>
-    </ul>
-</nav><!-- / nav -->
+                    
+                <div class="login-wrap">
+                    <div class="login-html">
+                            <h1>Iniciar Sesion</h1>
+                            </div>
+                        </div>
+                <div class="login-form">
+                    <div class="sign-in-htm">
+                        <div class="group">
+                            <label for="user" class="label">Nombre de usuario</label>
+                            <input id="user" type="text" class="input">
+                        </div>
+                        <div class="group">
+                            <label for="pass" class="label">Contraseña</label>
+                            <input id="pass" type="password" class="input" data-type="password">
+                        </div>
+                        <div class="group">
+                            <input id="check" type="checkbox" class="check" checked>
+                            <label for="check"><span class="icon"></span> Mantenerme conectado</label>
+                        </div>
+                        <div class="group">
+                            <input type="submit" class="button" value="Registrarse">
+                        </div>
+                        <div class="hr"></div>
+                        <div class="foot-lnk">
+                            <a href="#forgot">
+                                ¿Has olvidado tu contraseña?</a>
+                        </div>
+                    </div>
 
-</header><!-- / #main-header -->
-
-    
-<div class="login-wrap">
-    <div class="login-html">
-    <div class="container">
-            <h1>Iniciar Sesion</h1>
-            </div>
-        </div>
-<div class="login-form">
-    <div class="sign-in-htm">
-        <div class="group">
-            <label for="user" class="label">Nombre de usuario</label>
-            <input id="user" type="text" class="input">
-        </div>
-        <div class="group">
-            <label for="pass" class="label">Contraseña</label>
-            <input id="pass" type="password" class="input" data-type="password">
-        </div>
-        <div class="group">
-            <input id="check" type="checkbox" class="check" checked>
-            <label for="check"><span class="icon"></span> Mantenerme conectado</label>
-        </div>
-        <div class="group">
-            <input type="submit" class="button" value="Registrarse">
-        </div>
-        <div class="hr"></div>
-        <div class="foot-lnk">
-            <a href="#forgot">
-                ¿Has olvidado tu contraseña?</a>
-        </div>
-    </div>
-
-</body>
+        </body>
     </html>
-
-
--- ProyectoInventario/Registro.php
+++ ProyectoInventario/Registro.php
@@ -1,16 +1,47 @@
 
+<?php 
+        require 'BaseDatos.php';
+
+        //Variable Global se llenara al momento de ser ejecutado
+        $message = '';
+
+        //condiciones para los campos si no estan vacios puede agregar a la base
+    //variables Locales
+
+        if (!empty($_POST['email']) && !empty($_POST['password'])) {
+            $sql = "INSERT INTO usuarios (email, password) VALUES (:email, :password)";
+            $stmt = $conn->prepare($sql);//consulta de datos
+            $stmt->bindParam(':email', $_POST['email']);//vinculacion de datos
+            //$password = password_hash($_POST['password'], PASSWORD_BCRYPT);//Guardar variable con cifrado los datos
+            //almacenamos en variable password
+            $stmt->bindParam(':password', $password);
+        
+            
+        //condicion que todo se esta ejectando b
+        if ($stmt->execute()) {
+            $message = 'Nuevo usuario creado con éxito';
+        } else {
+            $message = 'Lo sentimos, debe haber habido un problema al crear su cuenta.';
+        }
+        }
+
+?>
 <!DOCTYPE html>
-<html lang="en">
-    <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Registrarse</title>
-            <link rel = "preconnect" href = "https://fonts.gstatic.com">
-        <link href = "https://fonts.googleapis.com/css2? family = Noto + Sans + JP: wght @ 300 & display = swap "rel =" stylesheet ">
-         <link rel="stylesheet" href="assets/css/style.css">     
-    </head>
-    <body>
+    <html lang="en">
+        <head>
+                <meta charset="UTF-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Registrarse</title>
+                <link rel = "preconnect" href = "https://fonts.gstatic.com">
+            <link href = "https://fonts.googleapis.com/css2? family = Noto + Sans + JP: wght @ 300 & display = swap "rel =" stylesheet ">
+            <link rel="stylesheet" href="assets/css/style.css">     
+        </head>
+        <body>
+
+            <?php if(!empty($message)): ?>
+            <p> <?= $message ?></p>
+            <?php endif; ?>
 
         <header id="main-header">
                     
@@ -53,11 +84,11 @@
                     <div class="hr"></div>
                     <div class="foot-lnk">
                         <label for="tab-1">¿Ya tienes cuenta?</a><br>
-                        <span> or <a href="Login.php">Iniciar Sesion</a></span>
+                        <span> o <a href="Login.php">Iniciar Sesion</a></span>
                     </div>
 
         </form>
 
             
         </body>
-</html>
+    </html>
